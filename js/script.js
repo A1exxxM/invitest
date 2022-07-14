4b5b59439d7592e0f5537e4b1458255dab74b05d
A1exxxM <93865128+A1exxxM@users.noreply.github.com>
2022-07-14 22:09:32 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -96,15 +96,46 @@
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 function animateHeader() {
-  const navBlack = document.querySelector('nav');
+  const navbar = document.querySelector('nav');
 
   function headerClass() {
-    navBlack.classList.remove('visible');
-    navBlack.classList.add('animate__animated');
-    navBlack.classList.add('animate__fadeInDown');
+    navbar.classList.remove('visible');
+    navbar.classList.add('animate__animated');
+    navbar.classList.add('animate__fadeInDown');
   }
 
   setTimeout(headerClass, 1500);
+  const photo = document.querySelector('#photo'),
+        products = document.querySelector('#products'),
+        contacts = document.querySelector('#contacts'),
+        photoScrollPoint = document.querySelector('.slider'),
+        productsScrollPoint = document.querySelector('.products'),
+        contactsScrollPoint = document.querySelector('.contacts'),
+        links = document.querySelectorAll('.header__list-item');
+
+  function scrollWeb(selector, scrollPoint) {
+    selector.addEventListener('mouseenter', () => {
+      links.forEach(item => {
+        item.style.color = '#000';
+      });
+      navbar.style.backgroundColor = '#fff';
+    });
+    selector.addEventListener('mouseleave', () => {
+      links.forEach(item => {
+        item.style.color = '#fff';
+      });
+      navbar.style.backgroundColor = '#000';
+    });
+    selector.addEventListener('click', () => {
+      scrollPoint.scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+  }
+
+  scrollWeb(photo, photoScrollPoint);
+  scrollWeb(products, productsScrollPoint);
+  scrollWeb(contacts, contactsScrollPoint);
 }
 
 /* harmony default export */ __webpack_exports__["default"] = (animateHeader);
